7447557d0c4f86f4d264c4555f97661deebb790b
Nayankumar <shettynayankuamr123@gmail.com>
2023-07-09 20:19:03 +0530

    nay

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,41 @@
+
+import {AiFillDelete} from "react-icons/ai"
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/cartSlice";
+import { toast } from "react-hot-toast";
+
+const CartItem = ({item, itemIndex}) => {
+  const dispatch = useDispatch();
+
+  const removeFromCart = () => {
+    dispatch(remove(item.id));
+    toast.success("Item Removed");
+  }
+
+  return (
+    <div className="flex items-center p-2 md:p-5 justify-between   mt-2 mb-2 md:mx-5 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] rounded-2xl hover:shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] hover:scale-105 transition duration-300 ease-in">
+      <div className="flex flex-col md:flex-row p-0 md:p-3 gap-5 items-center">
+        <div className="w-[30%]">
+          <img className="object-cover " src={item.image} />
+        </div>
+        <div className="md:ml-10 self-start space-y-5 w-[100%] md:w-[70%]">
+          <h1 className="text-xl text-slate-700 font-semibold">{item.title}</h1>
+          <h1 className="text-base text-slate-700 font-medium">{item.description.split(" ").slice(0,10).join(" ")+"..."}</h1>
+          <div className="flex items-center justify-between">
+            <p className="font-bold text-lg text-green-600">${item.price}</p>
+            <div className="text-red-800  bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
+            onClick={removeFromCart}>
+              <AiFillDelete/>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className='flex justify-center flex-col h-full'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
